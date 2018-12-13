c0bd8b06f0533bb549e464895b1d9707b0071b75
juan pablo <morales.juan.rodriguez@gmail.com>
2018-12-13 17:03:41 -0600

    creacion de sercios para consumir api

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,59 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-  </div>
+<template lang ="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+          type="text",
+          placeholder="Buscar canciones",
+          v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+      .container
+        p
+          small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="t in tracks")
+            | {{ t.name }} - {{ t.artists[0].name}}
 </template>
 
 <script>
+  import musicService from './services/track'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hola Vue!'
+        searchQuery: '',
+        tracks: []
+       }
+  },
+  methods: {
+    search () {
+        if (!this.searchQuery) {return}
+        musicService.search(this.searchQuery)
+            .then(res => {
+                this.tracks = res.tracks.items
+            })
+
     }
+  },
+  computed: {
+      searchMessage () {
+          return `Encontrados: ${this.tracks.length}`
+      }
   }
 }
 </script>
 
 <style lang="scss">
+  @import './src/scss/main.scss';
+
+  .results {
+    margin-top: 50px;
+  }
 
 </style>
