705edda2729991be8dcffc7da34c08f277af194b
juan pablo <morales.juan.rodriguez@gmail.com>
2018-12-17 12:14:09 -0600

    integracion de filtros

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from '@/App.vue'
 import EventBus from '@/plugins/event-bus'
 import VueRouter from 'vue-router'
 import routes from '@/routes'
-
+import msToMm from '@/filters/ms-to-mm'
 Vue.use(EventBus)
 Vue.use(VueRouter)
+Vue.use(msToMm)
 
 const router = new VueRouter({routes, mode: 'history'})
 new Vue({
